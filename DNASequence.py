6c564f72b773bae7e4bba2844c42eebd6dc05707
Dan Brown <dvbrown@student.unimelb.edu.au>
2013-10-03 14:02:24 +1000

    tested out window function well

--- DNASequence.py
+++ DNASequence.py
@@ -21,8 +21,11 @@ class DNAsequence:
         >>> exact_case = DNAsequence(exact_length_read)
         >>> exact_case.window_size()
         1
+        >>> float_read = DNAsequence(float_read)
+        >>> float_read.window_size()
+        3
         '''
-    #get the read length as an integer. What about rounding issues?
+    #get the read length as an integer. Rounds down using the int function
         read_length = len(self.dna_str)
         window_length = int(0.1 * read_length)
         if window_length < 1:
@@ -31,19 +34,29 @@ class DNAsequence:
         return window_length
     
     def trim(self,start,end):
-        '''Trim the sequence from start to end
+        '''Trim the sequence from start to end. Takes as input 2 integers
         
-        >>> DNAsequence.trim()
+        #>>> test_read = DNAsequence(read)
+        #>>> test_read.trim(start_no_cut, end_no_cut)
+        'NTTTTTCCTTCCTTTAAAAC'
+        #>>> test_read = DNAsequence(read)
+        #>>> test_read.trim(start_small_cut, end_small_cut)
+        'TCCTTCCTTTA'
         '''
-        pass
-        #return trimmed_dna_str
-        
+        self.dna_str = self.dna_str[start:end+1]
 
 
 if __name__ == '__main__':
-    #some dummy sequencing reads. One read under threshold of 1, one over 1.
+    #Test window function with some dummy sequencing reads.
+    #One read under threshold of 1, one over 1.
     short_read = 'TTTAAAAC'
-    long_read = 'NTTTTTCCTTCCTTTAAAAC'
+    long_read = 'TTTAAAACTTTAAAACGTAC'
+    float_read = 'TCCTTCCTTTAAAATCCTTCCTTTAAAATCCTTTAG'
     exact_length_read = 'CCTTTAAAAC'
+    
+    #Testing trimming function with different positions for starting and ending trim
+    read = 'NTTTTTCCTTCCTTTAAAAC'
+    start_no_cut, end_no_cut = 0, 20
+    start_small_cut, end_small_cut = 5, 15
     
     doctest.testmod()
